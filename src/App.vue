c76b5c8a48ec76ba8217a7374788e5fe2baf26e9
Bearfinn <kritsada.stw@gmail.com>
2020-01-02 11:58:18 +0700

    Add quiz block

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,123 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-toolbar-title>Pupe BNK48 Quiz</v-toolbar-title>
     </v-app-bar>
-
     <v-content>
-      <HelloWorld />
+      <v-container fill-height fluid>
+        <v-row align="center" justify="center">
+          <v-col md="8">
+            <template v-if="isQuizStarted">
+              <v-card>
+                <v-card height="500px">
+                  <v-progress-linear
+                    :value="correctQuestionsPercentage * 100"
+                    :bufferValue="answeredQuestionsPercentage * 100"
+                  />
+
+                  <v-container
+                    fill-height
+                    loading
+                    class="d-flex align-content-center"
+                  >
+                    <v-card-title
+                      >{{ currentQuestion.no }}.
+                      {{ currentQuestion.question }}</v-card-title
+                    >
+                    <v-card-text>
+                      <v-radio-group v-model="selectedChoice">
+                        <v-radio
+                          v-for="(choice, index) in currentQuestion.choices"
+                          :value="choice"
+                          :label="choice"
+                          :key="index"
+                          color="primary"
+                        />
+                      </v-radio-group>
+                    </v-card-text>
+                    <v-card-actions>
+                      <v-btn class="primary" @click="checkAnswer"
+                        >ยืนยันคำตอบ
+                      </v-btn>
+                    </v-card-actions>
+                  </v-container>
+                  <v-snackbar v-model="correctAnswerSnackbar" color="success"
+                    >ถูกต้อง!
+                  </v-snackbar>
+                  <v-snackbar v-model="wrongAnswerSnackbar" color="error"
+                    >ผิด!
+                  </v-snackbar>
+                </v-card>
+              </v-card>
+            </template>
+            <template v-else
+              ><QuizIntroductionCard v-on:startQuiz="startQuiz"
+            /></template>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import QuizIntroductionCard from "./components/QuizIntroductionCard";
+import quiz from "./quiz";
 
 export default {
   name: "App",
 
-  components: {
-    HelloWorld
-  },
+  components: { QuizIntroductionCard },
 
   data: () => ({
-    //
-  })
+    isQuizStarted: false,
+    questionNumber: 0,
+    currentQuestion: {},
+    selectedChoice: "",
+    correctAnswerSnackbar: false,
+    wrongAnswerSnackbar: false,
+    correctQuestionsCount: 0
+  }),
+
+  computed: {
+    questions() {
+      return quiz;
+    },
+    answeredQuestionsPercentage() {
+      return this.questionNumber / this.questions.length;
+    },
+    correctQuestionsPercentage() {
+      return this.correctQuestionsCount / this.questions.length;
+    }
+  },
+
+  methods: {
+    startQuiz() {
+      this.isQuizStarted = true;
+      this.goToNextQuestion();
+    },
+
+    checkAnswer() {
+      if (this.selectedChoice !== "") {
+        if (
+          this.selectedChoice === this.questions[this.questionNumber - 1].answer
+        ) {
+          this.correctQuestionsCount += 1;
+          this.correctAnswerSnackbar = true;
+        } else {
+          this.wrongAnswerSnackbar = true;
+        }
+        this.goToNextQuestion();
+      }
+    },
+    goToNextQuestion() {
+      this.selectedChoice = "";
+      this.questionNumber += 1;
+      this.setQuestion();
+    },
+    setQuestion() {
+      this.currentQuestion = this.questions[this.questionNumber - 1];
+    }
+  }
 };
 </script>
---
+++ src/components/QuizIntroductionCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-card height="500px">
+    <v-container fill-height class="d-flex align-content-center">
+      <v-card-title>ยินดีต้อนรับสู่เกมตอบคำถาม Pupe BNK48</v-card-title>
+      <v-card-text>
+        มีคำถามทั้งหมด 10 คำถาม ตอนถูกเท่าไหร่ โดเนทเพิ่มให้เท่านั้น!
+      </v-card-text>
+      <v-card-actions>
+        <v-btn class="primary" @click="$emit('startQuiz')">เริ่มเลย</v-btn>
+      </v-card-actions>
+    </v-container>
+  </v-card>
+</template>
+<script>
+export default {
+  name: "QuizIntroductionCard"
+};
+</script>
